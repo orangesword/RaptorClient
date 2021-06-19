a5a3e2a9ca1a71c6f1d8493fe88667a47ad10f25
RitomG <ritomgamer123@gmail.com>
2021-06-19 15:39:30 +0530

    completed 1.1 fix

---
+++ src/main/java/com/raptordev/raptor/client/module/modules/client/Console.java
@@ -0,0 +1,9 @@
+package com.raptordev.raptor.client.module.modules.client;
+
+import com.raptordev.raptor.client.module.Category;
+import com.raptordev.raptor.client.module.Module;
+
+@Module.Declaration(category = Category.CLIENT, name = "Console")
+public class Console extends Module {
+	
+}
